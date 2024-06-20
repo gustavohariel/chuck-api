ece7843ce18a5f5ddc243e61d23b3e0afd9f6e6c
Gustavo Hariel <78812552+gustavohariel@users.noreply.github.com>
2024-06-20 20:02:12 -0300

    update app.module imports

--- src/infra/modules/app.module.ts
+++ src/infra/modules/app.module.ts
@@ -1,10 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { ChuckModule } from './chuck.module';
 
-
 @Module({
-  imports: [ChuckModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    ChuckModule,
+  ],
 })
 export class AppModule {}
