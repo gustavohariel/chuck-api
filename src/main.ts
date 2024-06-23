c79ae4d4d079bb40461ec9fb265ad718c24efa02
Gustavo Hariel <78812552+gustavohariel@users.noreply.github.com>
2024-06-23 03:29:04 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './infra/modules/app.module';
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
